5a267784ea5b3a6d7f0872686bb1778f2749f0e7
Jemson <jemsonmislani@gmail.com>
2026-06-22 11:26:43 +0800

    uninstall jsonwebtoken and bcrypt to frontend, install it to backend. set up postgres connection.

--- lac-backend/server.js
+++ lac-backend/server.js
@@ -9,6 +9,14 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+const pool = new Pool({
+    user: 'postgres',
+    password: 'Im_Jem23*',
+    host: 'localhost',
+    database: 'lyricsandchords_library_fullstack',
+    port: 5432
+})
+
 const PORT = 3005;
 app.listen(PORT, () => {
     console.log(`Jem! Your server is running on port ${PORT}.`)
