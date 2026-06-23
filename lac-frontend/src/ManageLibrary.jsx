f1a67075a61d3fd2a1be9e56782e8250f26bc64d
Jemson <jemsonmislani@gmail.com>
2026-06-23 16:34:09 +0800

    create a layout and design to manage library, get api from backend using axios to get data and display it to page using .map.

--- lac-frontend/src/ManageLibrary.jsx
+++ lac-frontend/src/ManageLibrary.jsx
@@ -1,8 +1,78 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { FaEdit, FaTrash } from "react-icons/fa";
 
 export default function ManageLibrary(){
+    const [songlists, setSonglists] = useState([])
+
+        useEffect(() => {
+        const token = localStorage.getItem('token') || sessionStorage.getItem('token')
+        axios.get('http://localhost:3005/getDataOfSong', {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .then(result => {
+            setSonglists(result.data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }, [])
+
     return(
         <>
-            <h1>Manage here</h1>
+            <div className="flex h-screen bg-gray-100">
+                <aside className="w-64 bg-gray-900 text-white flex flex-col">
+                <div className="text-2xl font-bold p-6 border-b border-gray-700">Admin Dashboard</div>
+                <nav className="flex-1 p-4 space-y-2">
+                    <Link 
+                        to={'/adminDashboard'}
+                        className="block px-4 py-2 rounded hover:bg-gray-700">🏠 Home</Link>
+                    <Link 
+                        to={'/songlists'}
+                        className="block px-4 py-2 rounded hover:bg-gray-700">🎧 Song lists</Link>
+                    <Link 
+                        to={'/addsong'}
+                        className="block px-4 py-2 rounded hover:bg-gray-700">➕ Add songs<label className="text-white m-1 p-1"></label></Link>
+                    <a 
+                        to={'/manageLibrary'}
+                        className="block px-4 py-2 rounded hover:bg-gray-700">🙍🏻‍♂️ Manage Library</a>
+                </nav>
+                <div className="p-4 border-t border-gray-700 text-sm text-gray-400">
+                © 2026 Jemson Mislani
+                </div>
+                </aside>
+                <main className="flex-1 p-6 overflow-auto">
+                <h1 className="text-2xl font-semibold mb-5">Manage Songs</h1>
+                    <div className="grid gap-4" style={{ gridTemplateColumns: 'repeat(auto-fit, minmax(3in, max-content))' }}>
+                    </div>
+                    <div className="grid grid-cols-4 gap-2 mb-2 text-black text-l">
+                        <span className="font-bold">Title:</span>
+                        <span className="font-bold">Artist:</span>
+                        <span className="font-bold">Key Of:</span>
+                        <span className="font-bold">Action:</span>
+                    </div>
+                        {
+                            songlists.map((sl) => (
+                                <div key={sl.id}
+                                    className="grid grid-cols-4 gap-1 mb-1">
+                                    <div className="bg-gray-900 text-white p-2 font-medium rounded ">{sl.title}
+                                    </div>
+                                    <div className="bg-gray-900 text-white p-2 font-medium rounded">{sl.artist}
+                                    </div>
+                                    <div className="bg-gray-900 text-white p-2 font-medium rounded">{sl.song_key}
+                                    </div>
+                                    <div className="flex justify-center items-center gap-3 bg-sky-900 text-white p-2 font-medium rounded">
+                                        <span className="text-blue-400 hover:text-blue-300"><FaEdit /></span>
+                                        <span className="text-red-400 hover:text-red-400"><FaTrash /></span>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                </main>
+            </div>
         </>
     );
 }
