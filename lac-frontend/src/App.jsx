bbeb424d511e2b94756a8619ec7cb64284002b1e
Jemson <jemsonmislani@gmail.com>
2026-06-25 17:19:08 +0800

    get rid unuseful component, remake userdashboard component for user, do route. create a dasboard for user. style it.

--- lac-frontend/src/App.jsx
+++ lac-frontend/src/App.jsx
@@ -2,23 +2,28 @@ import { BrowserRouter, Routes, Route, } from 'react-router-dom'
 import LoginAccount from './LoginAcc'
 import RegisterAccount from './RegisterAcc'
 import AdminDashBoard from './AdminDashboard'
-import UserDashBoard from './UserDashboard'
 import Songlists from './Songlists'
 import AddDataOfSongs from './AddDataOfSong'
 import ManageLibrary from './ManageLibrary'
+import UserDashBoard from './users/UserDashboard'
 export default function App() {
 
   return (
     <>
       <BrowserRouter>
       <Routes>
+        {/* For creating and login account */}
         <Route path='/' element={<LoginAccount />}/>
         <Route path='/register' element={<RegisterAccount />}/>
+
+        {/* For admin */}
         <Route path='/adminDashboard' element={<AdminDashBoard />}/>
-        <Route path='/userDashboard' element={<UserDashBoard />}/>
         <Route path='/songlists' element={<Songlists />}/>
         <Route path='/addsong' element={<AddDataOfSongs />}/>
         <Route path='/manageLibrary' element={<ManageLibrary />}/>
+        
+        {/* For user */}
+        <Route path='userDashboard' element={<UserDashBoard/>}/>
       </Routes>
       </BrowserRouter>
     </>
--- lac-frontend/src/UserDashboard.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function UserDashBoard(){
-    
-    return(
-        <>
-            <h1>User DashBoard</h1>
-        </>
-    );
-}
